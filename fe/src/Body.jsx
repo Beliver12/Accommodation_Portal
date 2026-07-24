7bd5ee25ded0595db7a9868ec34c16ac828a1d4e
Nikola <nzencic@gmail.com>
2026-07-24 13:49:10 +0200

    Add actual images to the homepage
    Add a responsive image wrapper.
    
    This allows the images to scale properly across different screen sizes and prevents them from appearing stretched or blurry.

--- fe/src/Body.jsx
+++ fe/src/Body.jsx
@@ -1,20 +1,20 @@
-import reactsvg from "./assets/react.svg";
+import houseImage from "../public/Screenshot from 2026-07-24 12-41-02.png";
 
 function Body() {
   return (
     <div className="body">
       <div className="wrapper">
         <div className="card1">
-          <img src={reactsvg} alt="" />
+          <img src={houseImage} width='100%' height='100%'  alt="" />
         </div>
         <div className="card2">
-          <img src={reactsvg} alt="" />
+          <img src={houseImage} width='100%' height='100%' alt="" />
         </div>
         <div className="card3">
-          <img src={reactsvg} alt="" />
+          <img src={houseImage} width='100%' height='100%'  alt="" />
         </div>
         <div className="card4">
-          <img src={reactsvg} alt="" />
+          <img src={houseImage} width='100%' height='100%'  alt="" />
         </div>
       </div>
     </div>
